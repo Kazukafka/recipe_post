e4c50998bd56aa2944023aec453d1437cb537314
Kazukafka <pasuwaado135@gmail.com>
2021-09-15 11:30:19 +0900

    Basic Design Fin

---
+++ recipe/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+# Create your models here.
+# ここから追加
+
+
+class Recipe(models.Model):
+    title = models.CharField(verbose_name="タイトル", max_length=200)
+    content = models.TextField(verbose_name="内容")
+
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = "レシピ"
+        verbose_name_plural = "レシピ"
+
+    def __str__(self):
+        return self.title
+# 追加ここまで
